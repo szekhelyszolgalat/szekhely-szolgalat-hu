0b84c3634fb0564a95904780624ac41aa3613273
Szabolcs Bokonyi <szabcsee@gmail.com>
2025-09-05 09:59:54 +0200

    Fixing url config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://szekhelyszolgalat.github.io/szekhely-szolgalat-hu',
-  base: '/szekhely-szolgalat-hu',
+  base: '/',
   output: 'static',
   build: {
     format: 'directory'
