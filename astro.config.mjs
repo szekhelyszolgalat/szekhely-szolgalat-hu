5029fc70cae5e3748c6a8fe2a016e5ea365a5d0b
Szabolcs Bokonyi <szabcsee@gmail.com>
2025-09-05 10:14:59 +0200

    Site and base url fix.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://szekhelyszolgalat.github.io',
-  base: '/szekhely-szolgalat-hu',
+  site: 'https://szekhelyszolgalat.github.io/szekhely-szolgalat-hu',
+  base: '/szekhely-szolgalat-hu/',
   output: 'static',
 
   build: {
