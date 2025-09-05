f9251663cbec4ad3a05e32c3da2d82b919524c10
Szabolcs Bokonyi <szabcsee@gmail.com>
2025-09-05 10:06:26 +0200

    CSS fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://szekhelyszolgalat.github.io/szekhely-szolgalat-hu',
   base: '/',
   output: 'static',
+
   build: {
     format: 'directory'
-  }
+  },
+
+  integrations: [sitemap()]
 });
